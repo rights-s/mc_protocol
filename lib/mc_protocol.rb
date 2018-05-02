f4c0b5b7601aa2745262be6463b3dce65d3033fc
uchida <uchida@rights-s.jp>
2018-05-02 18:30:42 +0900

    エラーメッセージのI18n対応 エラークラス実装

--- lib/mc_protocol.rb
+++ lib/mc_protocol.rb
@@ -3,6 +3,14 @@ require "mc_protocol/client"
 require "mc_protocol/device"
 require "mc_protocol/config"
 require "active_support/all"
+require "i18n"
+
+# I18n
+I18n.enforce_available_locales = true if I18n.respond_to?(:enforce_available_locales=)
+I18n.available_locales = [:en, :ja]
+I18n.default_locale = :ja
+I18n.load_path.append Dir[File.join(File.dirname(__FILE__), 'mc_protocol', 'config', 'locales', '*.yml')]
+I18n.reload! if I18n.backend.initialized?
 
 module McProtocol
 end
--- mc_protocol.gemspec
+++ mc_protocol.gemspec
@@ -30,6 +30,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "activesupport"
+  spec.add_dependency "i18n"
 
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
